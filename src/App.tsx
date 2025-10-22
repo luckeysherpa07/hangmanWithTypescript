bd5b1a73d90f5ea3dca85607762ae91b6b5ecf57
Luckey <luckeysherpa07@gmail.com>
2025-10-22 20:16:43 +0200

    added HangmanBody in the Drawing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import words from "./wordList.json"
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
 
 function App() {
     const [wordToGuess, setWordToGuess] = useState(() => {
@@ -22,7 +25,7 @@ function App() {
             <div style={{ fontSize: "2rem", textAlign: "center" }}>
                 Lose Win
             </div>
-            <HangmannDrawing />
+            <HangmanDrawing />
             <HangmanWord />
             <Keyboard />
         </div>
