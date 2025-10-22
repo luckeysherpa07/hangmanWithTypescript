d02458f3cf18b3aaf82d05473bb8afe89a1c4d78
Luckey <luckeysherpa07@gmail.com>
2025-10-22 20:03:13 +0200

    added empty component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { useState } from "react"
+import words from "./wordList.json"
+
 function App() {
-    const [wordToGuess, setWordToGuess] = useState("Test")
-    return <h1>Hi</h1>
+    const [wordToGuess, setWordToGuess] = useState(() => {
+        return words[Math.floor(Math.random() * words.length)]
+    })
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+    console.log(wordToGuess)
+    return (
+        <div
+            style={{
+                maxWidth: "800px",
+                margin: "0 auto",
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                alignItems: "center",
+            }}
+        >
+            <div style={{ fontSize: "2rem", textAlign: "center" }}>
+                Lose Win
+            </div>
+            <HangmannDrawing />
+            <HangmanWord />
+            <Keyboard />
+        </div>
+    )
 }
 
 export default App
