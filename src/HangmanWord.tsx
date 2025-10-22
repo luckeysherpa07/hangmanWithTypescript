b4cb75c68a0c6272f04268615a8a4a52cdd7c36a
Luckey <luckeysherpa07@gmail.com>
2025-10-22 20:25:14 +0200

    added hangmanWord component

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,1 +1,37 @@
-export function HangmanWord() {}
+export function HangmanWord() {
+    const word = "hangman"
+    const guessedLetters = ["h", "a", "n"]
+    return (
+        <div
+            style={{
+                display: "flex",
+                gap: ".25em",
+                fontSize: "6rem",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                fontFamily: "monospace",
+            }}
+        >
+            {word.split("").map((letter, index) => (
+                <span
+                    key={index}
+                    style={{
+                        borderBottom: ".1em solid black",
+                        width: "1ch",
+                        textAlign: "center",
+                    }}
+                >
+                    <span
+                        style={{
+                            visibility: guessedLetters.includes(letter)
+                                ? "visible"
+                                : "hidden",
+                        }}
+                    >
+                        {letter}
+                    </span>
+                </span>
+            ))}
+        </div>
+    )
+}
